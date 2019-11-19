1301026d4525d46e54c08c899f48b83f52d622f7
Marina <56669936+Polenova@users.noreply.github.com>
2019-11-19 18:42:56 +0100

    Update Utils.java

--- app/src/main/java/ru/android/polenova/Utils.java
+++ app/src/main/java/ru/android/polenova/Utils.java
@@ -7,9 +7,9 @@ public class Utils {
 
     private static int sTheme;
 
-    public final static int THEME_GREEN = 0;
-    public final static int THEME_BLACK = 1;
-    public final static int THEME_BLUE = 2;
+    public final static int THEME_SMALL = 0;
+    public final static int THEME_MEDIUM = 1;
+    public final static int THEME_BIG = 2;
 
     /**
      * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
@@ -28,14 +28,14 @@ public class Utils {
     public static void onActivityCreateSetTheme(Activity activity) {
         switch (sTheme) {
             default:
-            case THEME_GREEN:
-                activity.setTheme(R.style.ThemeGreen);
+            case THEME_SMALL:
+                activity.setTheme(R.style.Margin1);
                 break;
-            case THEME_BLACK:
-                activity.setTheme(R.style.ThemeBlack);
+            case THEME_MEDIUM:
+                activity.setTheme(R.style.Margin2);
                 break;
-            case THEME_BLUE:
-                activity.setTheme(R.style.ThemeBlue);
+            case THEME_BIG:
+                activity.setTheme(R.style.Margin3);
                 break;
         }
     }
