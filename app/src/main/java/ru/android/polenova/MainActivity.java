0972df594746670612d0e4e7b5d5ed281533ef04
Marina <M.V.Polenova@users.noreply.github.com>
2019-11-19 15:11:06 +0100

    hw3.4.1

--- app/src/main/java/ru/android/polenova/MainActivity.java
+++ app/src/main/java/ru/android/polenova/MainActivity.java
@@ -15,19 +15,19 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonOK;
     private Spinner spinnerLanguage;
+    private Spinner spinnerTheme;
     private Locale locale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(MainActivity.this);
         setContentView(R.layout.activity_main);
         initView();
         buttonOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Object language = spinnerLanguage.getSelectedItem();
-                String stringLang = language.toString();
-                switch (stringLang) {
+                switch (spinnerLanguage.getSelectedItem().toString()) {
                     case "Србски":
                         locale = new Locale("sr");
                         break;
@@ -42,14 +42,34 @@ public class MainActivity extends AppCompatActivity {
                 config.setLocale(locale);
                 getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
                 recreate();
+                switch (spinnerTheme.getSelectedItem().toString()) {
+                    case "Green":
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_GREEN);
+                        break;
+                    case "Black":
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLACK);
+                        break;
+                    case "Blue":
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLUE);
+                        break;
+                }
             }
         });
+
     }
 
     private void initView() {
         spinnerLanguage = findViewById(R.id.spinnerLang);
+        spinnerTheme = findViewById(R.id.spinnerTheme);
         buttonOK = findViewById(R.id.buttonOK);
         initSpinnerLanguage();
+        initSpinnerTheme();
+    }
+
+    private void initSpinnerTheme() {
+        ArrayAdapter<CharSequence> adapterTheme = ArrayAdapter.createFromResource(this, R.array.Theme, android.R.layout.simple_spinner_item);
+        adapterTheme.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTheme.setAdapter(adapterTheme);
     }
 
     private void initSpinnerLanguage() {
